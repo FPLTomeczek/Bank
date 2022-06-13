09125e863b506bcf1f8f5d342d7772fbe5834f69
FPLTomeczek <tomaszs18m@gmail.com>
2022-06-13 20:01:22 +0200

    final v7

--- src/main/java/pl/javaskills/creditapp/Main.java
+++ src/main/java/pl/javaskills/creditapp/Main.java
@@ -1,19 +1,23 @@
 package pl.javaskills.creditapp;
 
 import pl.javaskills.creditapp.client.ConsoleReader;
+import pl.javaskills.creditapp.core.CreditApplicationDecision;
 import pl.javaskills.creditapp.core.CreditApplicationService;
+import pl.javaskills.creditapp.core.PersonScoringCalculator;
 import pl.javaskills.creditapp.core.model.LoanApplication;
 
 
 public class Main {
     public static void main(String[] args) {
+
+        CreditApplicationService service = new CreditApplicationService(new PersonScoringCalculator());
         LoanApplication loanApplication = new ConsoleReader().readInputParameters();
         System.out.println("Hello, " + loanApplication.getPerson().getPersonalData().getName()
                 + " "
                 + loanApplication.getPerson().getPersonalData().getLastName()
                 + "!");
-        CreditApplicationService CAS = new CreditApplicationService();
-        String decision = CAS.getDecision(loanApplication);
+        CreditApplicationDecision CAD = service.getDecision(loanApplication);
+        String decision = CAD.getDecisionString();
         System.out.println(decision);
     }
 }
